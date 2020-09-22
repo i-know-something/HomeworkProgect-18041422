6e2110ba87fb18ea5055a072140575d98a3bbe7d
i-know-something <2848866234@qq.com>
2020-09-22 23:53:23 +0800

    0.2

---
+++ Cv_test/test.cpp
@@ -0,0 +1,21 @@
+#include <opencv2/opencv.hpp>
+
+#include <iostream>
+
+using namespace cv;
+
+int main()
+{
+	//read a local picture
+	//thr file path after imread can be changed to others
+
+	Mat img = imread("C:\\Users\\Administrator\\Desktop\\pictures\\1.png");
+
+	imshow("test", img);
+
+	//waiting for any key pressed
+
+	waitKey(0);
+
+	return 0;
+}
